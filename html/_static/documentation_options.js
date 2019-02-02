eab0af3fb2e4136f95f10d95e731fc2c9d19df6e
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-02-02 14:37:12 +0000

    Deploy eudoxos/saints to github.com/eudoxos/saints.git:gh-pages

--- html/_static/documentation_options.js
+++ html/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: 'd26ea29',
+    VERSION: 'b5d853d',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     FILE_SUFFIX: '.html',
